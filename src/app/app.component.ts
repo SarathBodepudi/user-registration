9dbed789b7a2878016c886bed8953cd5a5dc9b7f
Sarath Bodepudi <sarathbabubodepudi@gmail.com>
2022-04-21 23:55:03 -0500

    Screens Designed and Routes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <app-nav-bar></app-nav-bar>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: '../login/login.component.html'
+})
+
+export class LoginComponent {
+
+}
